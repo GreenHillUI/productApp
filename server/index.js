c473ab19d8f127999cd8f2ec01476dfe7fff9b82
Matthew Gilliam <moofus2000@gmail.com>
2022-07-07 11:56:29 -0700

    Added more skeleton code server might run now?

---
+++ server/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path');
+
+var app = express();
+
+app.use(express.static(__dirname + '../client/dist'));
+
+
+
+var port = 3000;
+
+app.listen(port, function() {
+  console.log(`listening on port ${port}`);
+});
+
