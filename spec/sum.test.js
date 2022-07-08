a1556305c33365b65d2bb9c6064b496797fc4094
Coty Janeway <coty.janeway@gmail.com>
2022-07-08 16:45:02 -0700

    Refactoring according to linter

--- spec/sum.test.js
+++ spec/sum.test.js
@@ -1,5 +1,8 @@
-const sum = require('./sum.js')
+/* eslint-disable no-undef */
+
+// removed .js extension from below according to linter
+const sum = require('./sum');
 
 test(`adds 1 + 2 to equal 3`, () => {
-  expect(sum(1,2)).toBe(3);
+  expect(sum(1, 2)).toBe(3);
 });
