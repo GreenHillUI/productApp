6e0847c8902a807250ad02a19623cc874eb458c3
Marshall Zmuda <marshall.z@outlook.com>
2022-07-11 09:09:00 -0700

    Implement QuestionList Component

---
+++ client/src/containers/QuestionListCont.js
@@ -0,0 +1,10 @@
+// import { connect } from 'react-redux';
+// import QuestionList from '../components/QuestionList';
+// const { expand } = require('../actions/QuestionListAct.jsx').expand;
+
+// const mapStateToProps = (storeData) => ({videos: storeData.videos});  //connects the prop to the state saved in the store
+// const mapDispatchToProps = (dispatch) => ({                           //links the event handler to the store via dispatch
+//   handleVideoListEntryTitleClick: (choice) => dispatch(changeVideo(choice))
+// });
+// QuestionList();
+// const VideoListContainer = connect(mapStateToProps, mapDispatchToProps)(VideoList);
