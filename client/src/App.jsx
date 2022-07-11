abe3a5841debbce0e4d279834e16265ba463bac0
Matthew Gilliam <moofus2000@gmail.com>
2022-07-11 01:34:07 -0700

    Implemented index.jsx in client directory index.jsx mounts the App component in App.jsx to the DOM that's it that's all it does

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import * as ReactRedux from 'react-redux';
+
+
+// stateful component
+function App() {
+  return (
+    <div>
+      {/* <ReactRedux.Provider store={store}> */}
+      Hello World!
+      {/* </ReactRedux.Provider> */}
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/index.jsx
@@ -0,0 +1,13 @@
+/**
+ * This file is run automatically when the website is painted for the first time
+ * it should not have to be changed. To add components modify App.jsx
+ */
+
+import { createRoot } from 'react-dom/client';
+import App from './App';
+
+console.log('help plz');
+
+const container = document.getElementById('app');
+const root = createRoot(container);
+root.render(<App />,);
