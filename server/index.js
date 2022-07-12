473d7395c26d7ad6a22ffdd00ba5155d0a9f9778
Matthew Gilliam <moofus2000@gmail.com>
2022-07-12 14:08:05 -0700

    moved route handling into server/index.js delted routes.js as it is now obsolete

--- server/index.js
+++ server/index.js
@@ -1,10 +1,17 @@
 const express = require('express');
-const routes = require('./routes');
+const controllers = require('./controllers');
 
 const app = express();
 
 app.use(express.static('client/dist'));
-app.use(routes);
+
+app.get('/products*', (req, res) => {
+  console.log('help ', req.path);
+  controllers.get(req.path)
+    .then((products) => {
+      res.status(200).send(products);
+    });
+});
 
 const port = 3000;
 
