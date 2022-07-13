895c0960dee642ab580fc138bdcf1d3273d46f2d
BlandSchwad <blandschwad@gmail.com>
2022-07-13 15:12:38 -0700

    Created reducer, implemented basic carousel

---
+++ client/src/components/Carousel.jsx
@@ -0,0 +1,67 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { FaArrowCircleLeft, FaArrowCircleRight } from 'react-icons/fa';
+
+//Takes an array of images and makes them carousel slides
+
+
+function Carousel({
+  selectedStyle, displayIndex, incrementDisplayIndex, decrementDisplayIndex
+}) {
+  
+  let displaySlides = [];
+  let displayThumbs = [];
+  const pictureData = selectedStyle.photos;
+
+  function makeSlide(image) {
+    return (<img className='slide' src={image.url} alt={`${selectedStyle.name} style`} />);
+  }
+
+  function makeThumbnailBar(image) {
+    return ((<img className='gallery-thumbnail' src={image.thumbnail_url} alt={`${selectedStyle.name} style`} />))
+  }
+
+  if (pictureData) {
+    displaySlides = pictureData.map(makeSlide);
+    displayThumbs = pictureData.map(makeThumbnailBar);
+  } 
+
+  function handleLeftArrowClick() {
+    if (displayIndex !== 0) {
+      decrementDisplayIndex(displayIndex--);
+    }    
+  }
+  function handleRightArrowClick() {
+    if (displayIndex !== selectedStyle.photos.length - 1) {
+      incrementDisplayIndex(displayIndex++);
+    }
+  }
+
+  return (
+    <div>
+      <div className="Container">
+        <div className="Display"> 
+          {displaySlides ? displaySlides[displayIndex] : <div> Loading </div>}
+          <FaArrowCircleLeft onClick={handleLeftArrowClick} />
+          <FaArrowCircleRight onClick={handleRightArrowClick} />
+        </div>
+          <div className='thumbnail-bar'>{/*displayThumbs*/}</div>
+      </div> 
+    </div>
+  );
+}
+
+const CarouselContainer = connect(
+  (state) => ({
+    selectedStyle: state.selectedStyle,
+    displayIndex: state.displayIndex
+  }),
+
+  (dispatch) => ({
+    setDisplayIndex: (num) => dispatch({ type: `SETDISPLAYINDEX`, displayIndex: num }),
+    incrementDisplayIndex: (num) => dispatch({ type: `INCREMENT`, displayIndex: num }),
+    decrementDisplayIndex: (num) => dispatch({ type: `DECREMENT`, displayIndex: num })
+  })
+)(Carousel);
+
+export default CarouselContainer;
