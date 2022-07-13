da050268baee67b23e22eca5fb3938485f147ea2
Marshall Zmuda <marshall.z@outlook.com>
2022-07-13 13:54:43 -0700

    Make CSS edits

--- client/src/components/Question.jsx
+++ client/src/components/Question.jsx
@@ -8,7 +8,7 @@ export default function Question() {
         <button type='button' id='q-a'>Add Answer</button>
         <button type='button' id='q-h'>Helpful? Yes (0) |</button>
       </span>
-      <span id='temp-a'>ANSWERS HERE</span>
+      <span id='temp-a'>A: Answer Text</span>
       <button id='a-load' type='button'>LOAD MORE ANSWERS</button>
     </div>
   );
