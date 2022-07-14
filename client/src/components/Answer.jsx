038edd8035758c988655751b5b18b81d7a33f979
Marshall Zmuda <marshall.z@outlook.com>
2022-07-14 14:25:21 -0700

    Delete extra comments in Answer.jsx

--- client/src/components/Answer.jsx
+++ client/src/components/Answer.jsx
@@ -3,19 +3,6 @@ import React from 'react';
 
 export default function Answer({ answer }) {
 
-  // const aFilter = useSelector((state) => (state.qList.qFilter));
-   //if getting questions of product in props
-  //gets a list of answers that contain the current filter, then maps their IDs
-  // debugger;
-  // const fitleredAs = answers.filter((a) => (a.body.indexOf(aFilter) !== -1)).map((a) => (a.answer_id));
-  // //Sorts the questions by helpfulness rating
-  // const sorted = questions.results.sort((a, b) => (a.question_helpfulness - b.question_helpfulness));
-  //Filters the sorted list to only Qs whose answers
-  // const filtered = (filter === '' ? sorted : sorted.filter((q) => ())
-
-
-
-
   return (
     <div className='answer'>
       <span className='a.icon'><b>A:</b></span>
