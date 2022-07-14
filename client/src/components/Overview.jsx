a1afd9e08b3e5292d06df8868b6d6c5518963d32
BlandSchwad <blandschwad@gmail.com>
2022-07-14 07:11:30 -0700

    Removed ComponentDidMount Function to start making helper

--- client/src/components/Overview.jsx
+++ client/src/components/Overview.jsx
@@ -7,77 +7,76 @@ import StyleSelector from "./StyleSelector";
 import AddToCart from './AddtoCart';
 
 
-function setDefaultStyle(styles) {
+
+function getDefaultStyle(styles) {
 
   const result = styles.filter((style) => style['default?'] === true);
  
   return result;
 }
 
-class Overview extends React.Component {
- 
+function Overview({ productInfo, selectedStyle }) {
+  
 
 
-  componentDidMount() {
-    const { setStyles, setSelectedStyle, setProductInfo } = this.props;
-    axios.get('/products/40348')
-      .then((response) => {
+  // componentDidMount() {
+  //   const { setStyles, setSelectedStyle, setProductInfo } = this.props;
+  //   const pingNum = 40348;
+  //   axios.get(`/products/${pingNum}`)
+  //     .then((response) => {
      
-        setProductInfo(response.data);
-      })
-      .catch(() => console.log(`Error loading product info`));
+  //       setProductInfo(response.data);
+  //     })
+  //     .catch(() => console.log(`Error loading product info`));
       
-    axios.get('/products/40348/styles')
-      .then((response) => {
-        setStyles(response.data.results);
-        setSelectedStyle(setDefaultStyle(response.data.results));
-      })
-      .catch((err) => console.log(err));
+  //   axios.get(`/products/${pingNum}/styles`)
+  //     .then((response) => {
+  //       setStyles(response.data.results);
+  //       setSelectedStyle(getDefaultStyle(response.data.results));
+  //     })
+  //     .catch((err) => console.log(err));
       
 
-  }
+  // }
   
 
 
   //Add section for product features if they exist
-  render() {
-    const { productInfo, selectedStyle } = this.props;
-
      
-    return (
-    //Coming back to image gallery after writing a carousel
-      <div>
+  return (
+  //Coming back to image gallery after writing a carousel
+    <div>
           
-        <h2>{productInfo ? productInfo.category : `Loading`}</h2>
-        <h1>
-          {productInfo ? productInfo.name : `Loading`} 
-        </h1>
-        <div>***IMAGE GALLERY PLACEHOLDER*** </div> 
-        <div>{productInfo ? productInfo.slogan : `Loading`}</div>
-        <div>
-          <p>
-            {productInfo ? productInfo.description : `Loading`} 
-          </p>
-        </div>
-
-        <div>
-          Styles:
-          <br />
-          <StyleSelector />
-          <div>
-            Review Score: 
-            {/*this.generateStars(this.reviewAverage())*/}
-          </div>
-        </div>
-        <div>
-          Price: 
-          {selectedStyle.sale_price ? `Was $${selectedStyle.original_price} Now: $${selectedStyle.sale_price}` : `$${selectedStyle.original_price}`}
-        </div>
-
-        <AddToCart />
+      <h2>{productInfo ? productInfo.category : `Loading`}</h2>
+      <h1>
+        {productInfo ? productInfo.name : `Loading`} 
+      </h1>
+      <div>***IMAGE GALLERY PLACEHOLDER*** </div> 
+      <div>{productInfo ? productInfo.slogan : `Loading`}</div>
+      <div>
+        <p>
+          {productInfo ? productInfo.description : `Loading`} 
+        </p>
       </div>
-    );
-  }
+
+      <div>
+        Styles:
+        <br />
+        <StyleSelector />
+        <div>
+          Review Score: 
+          {/*this.generateStars(this.reviewAverage())*/}
+        </div>
+      </div>
+      <div>
+        Price: 
+        {selectedStyle.sale_price ? `Was $${selectedStyle.original_price} Now: $${selectedStyle.sale_price}` : `$${selectedStyle.original_price}`}
+      </div>
+
+      <AddToCart />
+    </div>
+  );
+  
 
 }
 
@@ -90,7 +89,7 @@ const OverviewContainer = connect(
   }),
   // links the event handler to the store via dispatch
   (dispatch) => ({
-    setStyles: (styles) => dispatch({ type: 'SETALLSTYLES', styles: styles }),
+    setStyles: (styles) => dispatch({ type: 'SETALLSTYLES', styles }),
     setProductInfo: (info) => dispatch({ type: 'SETPRODUCTINFO', productInfo: info }),
     setSelectedStyle: (style) => dispatch({ type: 'SETSELECTEDSTYLE', selectedStyle: style })
   }),
