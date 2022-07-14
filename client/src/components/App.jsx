b76884f474c9edb3ccf79d1fa0828262aacbc476
BlandSchwad <blandschwad@gmail.com>
2022-07-14 08:29:43 -0700

    Removed axios requests and made Overview stateless function

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 //import containers next
 import QListContainer from '../containers/QListContainer';
 import Overview from './Overview';
+
 //NOTE: Should only have containers rendered inside, using QuestionList
 //component temporarily, will change either when done with CSS or when container is built
 
--- client/src/components/Overview.jsx
+++ client/src/components/Overview.jsx
@@ -1,4 +1,3 @@
-import axios from 'axios';
 import React from "react";
 import { connect } from 'react-redux';
 //import { FaFacebookSquare, FaInstagram, FaPinterest, FaTwitter } from 'react-icons/fa' ;
@@ -7,11 +6,6 @@ import StyleSelector from "./StyleSelector";
 import AddToCart from './AddtoCart';
 
 
-function setDefaultStyle(styles) {
-  //console.log(styles);
-  const result = styles.filter((style) => style['default?'] === true);
-  return result;
-}
 
 //Finds and returns average of reviews
 //TODO: Handle edge case for no reviews in accordance with business docs
@@ -45,75 +39,44 @@ function generateStars(average) {
 
   return starBar;
 }
-class Overview extends React.Component {
+function Overview({ productInfo, selectedStyle, metaData }) {
   
-
-  componentDidMount() {
-    const {
-      setStyles, setSelectedStyle, setProductInfo, setMetaData
-    } = this.props;
-    axios.get('/products/40348')
-      .then((response) => {
-        //console.log(response.data);
-        setProductInfo(response.data);
-      })
-      .catch(() => console.log(`Error loading product info`));
-      
-    axios.get('/products/40348/styles')
-      .then((response) => {
-        setStyles(response.data.results);
-        setSelectedStyle(setDefaultStyle(response.data.results));
-      })
-      .catch((err) => console.log(err));
-      
-    axios.get('/reviews/meta', { params: { product_id: 40348 } })
-      .then((response) => {
-        setMetaData(response.data.ratings);
-      })
-      .catch((err) => console.log(err));  
-  }
-    
-  //Add section for product features if they exist
-  render() {
-    const { productInfo, selectedStyle, metaData } = this.props;
-    
-    return (
+  //Add section for product features if they exist 
+  return (
     //Coming back to image gallery after writing a carousel
+    <div>
+      <h2>{productInfo ? productInfo.category : `Loading`}</h2>
+      <h1>
+        {productInfo ? productInfo.name : `Loading`} 
+      </h1>
+      <div>***IMAGE GALLERY PLACEHOLDER*** </div> 
+      <div>{productInfo ? productInfo.slogan : `Loading`}</div>
       <div>
-          
-        <h2>{productInfo ? productInfo.category : `Loading`}</h2>
-        <h1>
-          {productInfo ? productInfo.name : `Loading`} 
-        </h1>
-        <div>***IMAGE GALLERY PLACEHOLDER*** </div> 
-        <div>{productInfo ? productInfo.slogan : `Loading`}</div>
-        <div>
-          <p>
-            {productInfo ? productInfo.description : `Loading`} 
-          </p>
-        </div>
-
-        <div>
-          Styles:
-          <br />
-          <StyleSelector />
-          <div>
-            Review Score: 
-            {metaData ? generateStars(reviewAverage(metaData)) : `Loading`}
-            {/*this.generateStars(this.reviewAverage())*/}
-          </div>
-        </div>
-        <div>
-          Price: 
-          {selectedStyle.sale_price ? `Was $${selectedStyle.original_price} Now: $${selectedStyle.sale_price}` : `$${selectedStyle.original_price}`}
-        </div>
-
-        <AddToCart />
+        <p>
+          {productInfo ? productInfo.description : `Loading`} 
+        </p>
       </div>
-    );
-  }
 
+      <div>
+        Styles:
+        <br />
+        <StyleSelector />
+        <div>
+          Review Score: 
+          {metaData ? generateStars(reviewAverage(metaData)) : `Loading`}
+          {/*this.generateStars(this.reviewAverage())*/}
+        </div>
+      </div>
+      <div>
+        Price: 
+        {selectedStyle.sale_price ? `Was $${selectedStyle.original_price} Now: $${selectedStyle.sale_price}` : `$${selectedStyle.original_price}`}
+      </div>
+      <AddToCart />
+    </div>
+  );
 }
+
+
 
 const OverviewContainer = connect(
   // connects the props to the state saved in the store
