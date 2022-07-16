d6a0616ab12154853addbab2abc064b520b2a2f6
Matthew Gilliam <moofus2000@gmail.com>
2022-07-16 00:47:30 -0700

    implemented weighted average calculator for stars

--- client/src/components/Stars.jsx
+++ client/src/components/Stars.jsx
@@ -1,6 +1,23 @@
 import React from 'react';
 import { ImStarFull, ImStarEmpty } from 'react-icons/im';
 
+function calcWidth(ratings) {
+  let width = 5;
+  if (Array.isArray(ratings)) {
+    let total = 0;
+    let weightedSum = 0;
+    Object.keys(ratings).forEach((i) => {
+      total += Number(ratings[i]);
+      weightedSum += Number(i) * ratings[i];
+    });
+    width = weightedSum / total;
+  } else if (typeof ratings === 'number') {
+    width = ratings;
+  }
+  width = Math.round(width * 4) * 5; 
+  console.log(width / 20);
+  return { width: `${width}%` };
+}
 
 function Stars({ rating }) {
   return (
@@ -12,7 +29,7 @@ function Stars({ rating }) {
         <ImStarEmpty />
         <ImStarEmpty />
       </div>
-      <div className='star-rating'>
+      <div className='star-rating' style={calcWidth(rating)}>
         <ImStarFull />
         <ImStarFull />
         <ImStarFull />
