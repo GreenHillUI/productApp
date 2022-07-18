750848f10ca63153a671988f368305c36db094ae
Coty Janeway <coty.janeway@gmail.com>
2022-07-18 11:21:45 -0700

    Only show 2 view until "More Reviews" is clicked

--- client/src/components/ReviewsList.jsx
+++ client/src/components/ReviewsList.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Review from './Review';
 
 function ReviewsList({ results }) {
@@ -6,27 +6,50 @@ function ReviewsList({ results }) {
   // have state hook for how many reviews to show
   // when button is clicked, it uses counter += 2 to show 2 more
 
-  // if there are <3 reviews remaining, "More Reviews" button is not shown
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    console.log(count);
+  });
+
+
 
   return (
-    <div>
+    <div className="reviews-list">
       {/* This div houses all reviews to be shown */}
       <div>
         {`${results.length} reviews, sorted by...`}
       </div>
       <div>
         {/* loop through reviews? */}
-        {results.map((review) => (
-          <Review
-            id={review.review_id}
-            review={review}
-          />
-        ))}
+        {!count
+          ? results.slice(0, 2).map((review) => (
+            <Review
+              id={review.review_id}
+              review={review}
+            />
+          ))
+          : results.map((review) => (
+            <Review
+              id={review.review_id}
+              review={review}
+            />
+          ))}
       </div>
 
       {/* Buttons to show more or add reviews */}
-      <button className='review-button' type="button"> More Reviews </button>
+      {(!count ? (
+        <button
+          className='review-button'
+          type="button"
+          onClick={() => setCount(count + 1)}
+        >
+          Show More
+        </button>
+      ) : '')}
+
       <button className='review-button' type="button"> Add A Review + </button>
+
     </div>
   );
 }
