27177bd5c525ae9f834ff855ac68d8cbcf9ba0d6
Coty Janeway <coty.janeway@gmail.com>
2022-07-19 10:37:18 -0700

    Removed obsolete comments

--- server/index.js
+++ server/index.js
@@ -18,14 +18,12 @@ app.get('/products*', (req, res) => {
 
 app.get('/reviews/meta', (req, res) => {
   const config = { params: req.query };
-  // const config = { params: { product_id: 40348, count: 100, sort: 'newest' } };
   controllers.get(req.path, config).then((metaData) => {
     res.status(200).send(metaData);
   });
 });
 
 app.get('/reviews', (req, res) => {
-  // const config = { params: req.query };
   const config = { params: { product_id: 40348, count: 100, sort: 'relevant' } };
   controllers.get(req.path, config).then((metaData) => {
     res.status(200).send(metaData);
