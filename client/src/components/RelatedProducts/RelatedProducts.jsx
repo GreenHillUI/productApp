3ec9fdd4c0765495ba93ba5117e04f7a8bd95c70
Matthew Gilliam <moofus2000@gmail.com>
2022-07-21 16:58:16 -0700

    more styling for carrousel

--- client/src/components/RelatedProducts/RelatedProducts.jsx
+++ client/src/components/RelatedProducts/RelatedProducts.jsx
@@ -1,28 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import { connect } from 'react-redux';
 import Card from './ProductCard';
 
 const outfit = [];
 
-/**
- * Renders two related product carrousels
- * also provides semantic strucure for styling
- */
+
 function RelatedProductsComponent({ related }) {
+
+  const [offset, setOffset] = useState(0);
+  
   return (
     <section id="related-products">
+      <h1>Related Products</h1>
       <div className="product-carrousel">
-        <h1>Related Products</h1>
-        <ul>
+
+        <ul style={{ 'margin-left': -offset }}>
           {related.map((product) => <Card key={product.id} product={product} />)}
         </ul>
-        <button type="button" className="back">&#10094;</button>
-        <button type="button" className="foreward">&#10095;</button>
+
+        <button
+          type="button"
+          className="back"
+          onClick={() => setOffset(offset + 400)}
+        >
+          &#10094;
+        </button>
+
+        <button
+          type="button"
+          className="forward"
+          onClick={() => setOffset(offset - 400)}
+        >
+          &#10095;
+        </button>
       </div>
-      <h1>Your Outfit</h1>
+      {/* <h1>Your Outfit</h1>
       <ul className='product-carrousel'>
         {outfit.map((product) => <Card key={product.id} product={product} />)}
-      </ul>
+      </ul> */}
     </section>
   );
 }
