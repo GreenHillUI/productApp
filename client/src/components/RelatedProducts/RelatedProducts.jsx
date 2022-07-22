fbad975da514bb61713cae011ecfe503c141268f
Matthew Gilliam <moofus2000@gmail.com>
2022-07-22 02:44:12 -0700

    finished carrousel prototype

--- client/src/components/RelatedProducts/RelatedProducts.jsx
+++ client/src/components/RelatedProducts/RelatedProducts.jsx
@@ -1,43 +1,41 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { connect } from 'react-redux';
 import Card from './ProductCard';
-
-const outfit = [];
-
 
 function RelatedProductsComponent({ related }) {
 
   const [offset, setOffset] = useState(0);
+  const [size, setSize] = useState(0);
+  const [width, setWidth] = useState(0);
+
+  useEffect(() => {
+    try {
+      setSize(document.querySelector('.product-carrousel ul').childNodes.length);
+      setWidth(document.querySelector('.product-carrousel').offsetWidth);
+    } catch (e) { /* */ }
+  });
   
   return (
     <section id="related-products">
       <h1>Related Products</h1>
       <div className="product-carrousel">
 
-        <ul className="hide-before hide-after" style={{ 'margin-left': -offset }}>
+        <ul style={{ 'margin-left': -offset }}>
           {related.map((product) => <Card key={product.id} product={product} />)}
         </ul>
 
-        <button
-          type="button"
-          className="back"
-          onClick={() => setOffset(offset - 400)}
-        >
-          &#10094;
-        </button>
+        {offset ? (
+          <button aria-label="left" type="button" onClick={() => setOffset(offset - 320)}>
+            &#10094;
+          </button>
+        ) : null}
+        {offset + width < size * 320 ? (
+          <button aria-label="right" type="button" onClick={() => setOffset(offset + 320)}>
+            &#10095;
+          </button>
+        ) : null}
 
-        <button
-          type="button"
-          className="forward"
-          onClick={() => setOffset(offset + 400)}
-        >
-          &#10095;
-        </button>
       </div>
-      {/* <h1>Your Outfit</h1>
-      <ul className='product-carrousel'>
-        {outfit.map((product) => <Card key={product.id} product={product} />)}
-      </ul> */}
     </section>
   );
 }
